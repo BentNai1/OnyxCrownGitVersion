9621c5ba43b94259e700312deb21a902734f798a
Nathan Moyer <49211022+ncmoyer99@users.noreply.github.com>
2022-02-12 17:37:20 -0500

    --- Auto-Git Commit ---

--- Assets/Scripts/CombinationRotate.cs
+++ Assets/Scripts/CombinationRotate.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CombinationRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static event Action<string, int> Rotated = delegate { };
+    private bool coroutineAllowed;
+    private int numShown;
+     
+    private void Start()
     {
-        
+        coroutineAllowed = true;
+        numShown = 5;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if(coroutineAllowed)
+        {
+            StartCoroutine("RotateWheel");
+        }
+    }
+
+    private IEnumerator RotateWheel()
+    {
+        coroutineAllowed = false;
+
+        for(int i = 0; i <= 11; i++)
+        {
+            transform.Rotate(0f, 0f, -3f);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        coroutineAllowed = true;
+
+        numShown +=1;
+
+        if(numShown > 9)
+        {
+            numShown = 0;
+        }
+
+        Rotated(name, numShown);
     }
 }
