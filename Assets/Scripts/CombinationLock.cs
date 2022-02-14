83c9facf14d7ea9e8f0b1c83cedfcef2acf47627
Nathan Moyer <49211022+ncmoyer99@users.noreply.github.com>
2022-02-14 10:59:01 -0500

    Getting combination lock to work
    Had to make some scuffed combination wheels, and used probuilder since i couldn't find anything else. So i just mad an 8 sided combination wheel for testing.

--- Assets/Scripts/CombinationLock.cs
+++ Assets/Scripts/CombinationLock.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class CombinationLock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int[] result, answer;
+
     void Start()
     {
-        
+        result = new int[] { 5, 5, 5, 5};
+        answer = new int[] { 1, 2, 3, 4 };
+        CombinationRotate.Rotated += CheckResults;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckResults(string wheelname, int number)
     {
-        
+        switch (wheelname)
+        {
+            case "Wheel1":
+                result[0] = number;
+                break;
+
+            case "wheel2":
+                result[1] = number;
+                break;
+
+            case "wheel3":
+                result[2] = number;
+                break;
+
+            case "wheel4":
+                result[3] = number;
+                break;
+        }
+
+        if (result[0] == answer[0] && result[1] == answer[1] && result[2] == answer[2] && result[3] == answer[3])
+        {
+            Debug.Log("Gate Opened!");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CombinationRotate.Rotated -= CheckResults;
     }
 }
