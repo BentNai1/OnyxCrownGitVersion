f3a3243cf65ee2cf812728e16521242411c84f3c
Zevil009 <quickeljeremy@knights.ucf.edu>
2022-02-20 20:05:50 -0500

    Added the Cutscene scenes
    added the scenes for the cutscenes from L1 to L2 & L2 to L3.

---
+++ Assets/Scripts/SceneDelay/Intro.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+
+{
+	bool loadingStarted = false;
+	float secondsLeft = 0;
+	void Start()
+	{
+		StartCoroutine(DelayLoadLevel(46));
+	}
+	IEnumerator DelayLoadLevel(float seconds)
+	{
+		secondsLeft = seconds;
+		loadingStarted = true;
+		do
+		{
+			yield return new WaitForSeconds(1);
+		} 
+
+		while (--secondsLeft > 0);
+			Application.LoadLevel("Level1");
+	}
+	void OnGUI()
+	{
+	if (loadingStarted)
+	GUI.Label(new Rect(0, 0, 100, 20), secondsLeft.ToString());
+	}
+}
