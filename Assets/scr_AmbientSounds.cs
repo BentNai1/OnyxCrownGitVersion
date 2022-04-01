e90a9869ff24e3468cc6c3fb926b50c78e2395b9
Jimboz28 <70271665+Jimboz28@users.noreply.github.com>
2022-04-01 12:26:03 -0400

    --- Auto-Git Commit ---

--- Assets/scr_AmbientSounds.cs
+++ Assets/scr_AmbientSounds.cs
@@ -9,28 +9,46 @@ public class scr_AmbientSounds : MonoBehaviour
     [SerializeField] AudioClip ambientSound2;
     [SerializeField] AudioClip ambientSound3;
     int randomnumber;
-    // Start is called before the first frame update
+    bool play;
+    [SerializeField] float delaytime;
+    float delayTimer;
     void Start()
     {
-        
+        randomnumber = Random.Range (0,15);
+        audioSource.Play();
     }
-
     // Update is called once per frame
     void Update()
     {
-        randomnumber = Random.Range (0,15);
-        Debug.Log (randomnumber); 
-        if (randomnumber > 5 && randomnumber <= 10)
+        Debug.Log (randomnumber);
+        Debug.Log (delayTimer);
+        Debug.Log(play);
+            if (randomnumber > 5 && randomnumber <= 10)
         {
             audioSource.clip = ambientSound;
+            play = true;
         }
         if (randomnumber <= 5)
         {
             audioSource.clip = ambientSound2;
+            play = true;
         }
         if (randomnumber >= 10)
         {
-            audioSource.clip = ambientSound;
-        }
+            audioSource.clip = ambientSound3;
+            play = true;
+        } 
+       /* if (play)
+        {
+            audioSource.Play();
+            delayTimer -= Time.deltaTime;
+            if (delayTimer < 0)
+            {
+            randomnumber = Random.Range (0,15);
+            delayTimer = audioSource.clip.length + delaytime;
+            play = false;
+            }
+        }*/
+         
     }
 }
